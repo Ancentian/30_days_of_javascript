6c82018f057f1b792de2a11f448be824590d1890
ancentian <ancentmbithi8@gmail.com>
2023-02-14 21:38:25 +0300

    Initial Commit

---
+++ understanding-uninitialized-variables.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/*a should be defined and have a final value of 6.
+b should be defined and have a final value of 15.
+c should not contain undefined and should have a final value of the string I am a String!*/
+
+
+
---
+++ variables.js
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Defining Variables</h1>
+    <div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
